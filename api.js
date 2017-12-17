6bbe213d8c983f3331858c83dd15dc856aff12cd
Ashton Hellwig <hellwigashton@gmail.com>
2017-12-17 00:47:38 -0700

    Init

---
+++ api.js
@@ -0,0 +1,14 @@
+var jsonServer = require ('json-server')
+
+// Returns the Express.js server
+var server = jsonServer.create()
+
+// Default Middlewares
+server.use(jsonServer.defaults())
+
+var router = jsonServer.router('db.json')
+server.use(router)
+
+// Console log
+console.log('Listening at Port 4000 on 127.0.0.1')
+server.listen(4000)
